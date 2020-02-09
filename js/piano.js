59bf4535aa163467689880b92efab1fb1ad33ae7
senghor.khen <senghor.khen@PNC.LAN>
2020-02-09 19:20:31 +0700

    challenge-03

--- js/piano.js
+++ js/piano.js
@@ -1,11 +1,16 @@
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => {
-   key.addEventListener('click', () => {
-        console.log(key)
-   });
+   key.addEventListener('click', () => onPlaySound(key)); 
 });
 
-document.addEventListener('keydown', event => { // When click key all keyboard that we want to click it
-    const keyboard = event.key;
-    document.querySelector('button').innerHTML = keyboard;
-});
+// document.addEventListener('keydown', event => { // When click key all keyboard that we want to click it
+//     const keyboard = event.key;
+//     document.querySelector('button').innerHTML = keyboard;
+// });
+
+// function playSound
+function onPlaySound(key) {
+    const audioKey = document.getElementById(key.dataset.note);
+    audioKey.currentTime = 0;
+    audioKey.play();
+}
